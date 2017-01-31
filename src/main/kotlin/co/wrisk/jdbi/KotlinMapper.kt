b6908ef1532b8fb461f5038c4f8e68274a69f7cf
Alex Harin <alex@wrisk.co>
2017-01-31 11:57:53 +0000

    Backport KotlinMapper

--- src/main/kotlin/co/wrisk/jdbi/KotlinMapper.kt
+++ src/main/kotlin/co/wrisk/jdbi/KotlinMapper.kt
@@ -1,68 +1,129 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package co.wrisk.jdbi
 
 import org.skife.jdbi.v2.StatementContext
+import org.skife.jdbi.v2.tweak.ResultColumnMapper
 import org.skife.jdbi.v2.tweak.ResultSetMapper
 import java.lang.reflect.InvocationTargetException
 import java.sql.ResultSet
 import java.sql.SQLException
-import kotlin.reflect.KClass
-import kotlin.reflect.KParameter
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.*
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.javaType
-import kotlin.reflect.primaryConstructor
 
-class KotlinMapper<C : Any> internal constructor(private val clazz: Class<C>) : ResultSetMapper<C> {
-    private val kclass: KClass<C> = clazz.kotlin
+private typealias ValueProvider = (r: ResultSet, c: StatementContext) -> Any?
+private val NULL_VALUE_PROVIDER: ValueProvider = { _, _ -> null }
+
+
+class KotlinMapper<C : Any>(clazz: Class<C>) : ResultSetMapper<C> {
+
+    private val kClass: KClass<C> = clazz.kotlin
+
+    private val constructor = findConstructor(kClass)
+    private val constructorParameters = constructor.parameters
+    private val nullableMandatoryParameters = constructorParameters.filter { !it.isOptional && it.type.isMarkedNullable }
+    private val mutableProperties = kClass.memberProperties.map { it as? KMutableProperty1 }.filterNotNull()
+
+    private val propertyByColumnCache = ConcurrentHashMap<String, Optional<KMutableProperty1<C, *>>>()
+    private val parameterByColumnCache = ConcurrentHashMap<String, Optional<KParameter>>()
+
 
     @Throws(SQLException::class)
     override fun map(i: Int, rs: ResultSet, ctx: StatementContext): C {
-        try {
-            return tryMap(rs, ctx)
-        } catch (e: NoSuchMethodException) {
-            throw KotlinMemberAccessException(String.format("Unable to map %s entity", clazz), e)
-        } catch (e: InstantiationException) {
-            throw KotlinMemberAccessException(String.format("Unable to map %s entity", clazz), e)
-        } catch (e: IllegalAccessException) {
-            throw KotlinMemberAccessException(String.format("Unable to map %s entity", clazz), e)
-        } catch (e: InvocationTargetException) {
-            throw KotlinMemberAccessException(String.format("Unable to map %s entity", clazz), e)
+        val parameters = mutableMapOf<KParameter, ValueProvider>()
+        val properties = mutableListOf<Pair<KMutableProperty1<C, *>, ValueProvider>>()
+
+        val metadata = rs.metaData
+        val columns = (metadata.columnCount downTo 1).map { Pair(it, metadata.getColumnLabel(it)) }.distinctBy { (_, name) -> name.toLowerCase() }
+
+        for ((columnNumber, columnName) in columns) {
+
+            val parameter = parameterByColumnCache.computeIfAbsent(columnName, {
+                Optional.ofNullable(constructorParameters.find { matches(columnName, it.name) })
+            }).orElse(null)
+
+            if (parameter != null) {
+                parameters.put(parameter, valueProvider(parameter.type, columnNumber, ctx))
+                continue
+            }
+
+            val property = propertyByColumnCache.computeIfAbsent(columnName, {
+                Optional.ofNullable(mutableProperties.find { matches(columnName, it.name) })
+            }).orElse(null)
+
+            if (property != null) {
+                properties.add(Pair(property, valueProvider(property.returnType, columnNumber, ctx)))
+            }
         }
-
-    }
-
-    @Throws(NoSuchMethodException::class, InstantiationException::class, IllegalAccessException::class, InvocationTargetException::class, SQLException::class)
-    private fun tryMap(rs: ResultSet, ctx: StatementContext): C {
-        val constructor = kclass.primaryConstructor!!
-        constructor.isAccessible = true
-
-        // TODO: best fit for constructors + writeable properties, pay attention to nullables/optionals with default values
-        //       for now just call primary constructor using named params and hope
-
-        val validParametersByName = constructor.parameters.filter { it.kind == KParameter.Kind.VALUE && it.name != null }
-                .map { it.name!!.toLowerCase() to it }.toMap()
-
-        val matchingParms = (rs.metaData.columnCount downTo 1).map { rs.metaData.getColumnLabel(it).toLowerCase() }
-                .map { validParametersByName.get(it) }
-                .filterNotNull()
-                .map { param ->
-                    val paramType = param.type.javaType
-                    val columnMapper = ctx.columnMapperFor(paramType.erasedType()) ?: throw NoSuchColumnMapperException("No column mapper for " + paramType)
-                    Pair(param, columnMapper.mapColumn(rs, param.name, ctx))
-                }
-
-        val parmsThatArePresent = matchingParms.map { it.first }.toSet()
 
         // things missing from the result set that are Nullable and not optional should be set to Null
-        val nullablesThatAreAbsent = constructor.parameters.filter { !it.isOptional && it.type.isMarkedNullable && it !in parmsThatArePresent }.map {
-            Pair(it, null)
+        nullableMandatoryParameters.forEach { parameters.putIfAbsent(it, NULL_VALUE_PROVIDER) }
+
+        val parametersWithValue = parameters.mapValues { (_, valueProvider) -> valueProvider(rs, ctx) }
+        val propertiesWithValue = properties.map { (property, valueProvider) -> Pair(property, valueProvider(rs, ctx)) }
+
+        try {
+            constructor.isAccessible = true
+            val instance = constructor.callBy(parametersWithValue)
+
+            propertiesWithValue.forEach { (prop, value) ->
+                prop.isAccessible = true
+                prop.setter.call(instance, value)
+            }
+
+            return instance
+        } catch (e: InvocationTargetException) {
+            throw IllegalArgumentException("A bean, ${kClass.simpleName} was mapped which was not instantiable", e.targetException)
+        } catch (e: ReflectiveOperationException) {
+            throw IllegalArgumentException("A bean, ${kClass.simpleName} was mapped which was not instantiable", e)
         }
 
-        // things that are missing from the result set but are defaultable
-        val defaultableThatAreAbsent = constructor.parameters.filter { it.isOptional && !it.type.isMarkedNullable && it !in parmsThatArePresent }.toSet()
-
-        val finalParms = (matchingParms + nullablesThatAreAbsent)
-                .filterNot { it.first in defaultableThatAreAbsent }
-                .toMap()
-        return constructor.callBy(finalParms)
     }
+
+
+}
+
+private fun <C : Any> findConstructor(kClass: KClass<C>) = kClass.primaryConstructor ?: findSecondaryConstructor(kClass)
+
+private fun <C : Any> findSecondaryConstructor(kClass: KClass<C>): KFunction<C> {
+    if (kClass.constructors.size == 1) {
+        return kClass.constructors.first()
+    } else {
+        throw IllegalArgumentException("A bean, ${kClass.simpleName} was mapped which was not instantiable (cannot find appropriate constructor)")
+    }
+}
+
+private fun matches(columnName: String, paramName: String?): Boolean {
+    val param = paramName ?: return false
+    return columnName.toLowerCase() == param.toLowerCase()
+}
+
+
+private fun valueProvider(paramType: KType, columnNumber: Int, ctx: StatementContext): ValueProvider {
+    val columnMapper = ctx.columnMapperFor(paramType.javaType.erasedType()) ?: (object : ResultColumnMapper<Any> {
+        override fun mapColumn(r: ResultSet, columnNumber: Int, ctx: StatementContext): Any {
+            return r.getObject(columnNumber)
+        }
+
+        override fun mapColumn(r: ResultSet, columnLabel: String, ctx: StatementContext): Any {
+            return r.getObject(columnLabel)
+        }
+    })
+    return { r, c -> columnMapper.mapColumn(r, columnNumber, c) }
 }
